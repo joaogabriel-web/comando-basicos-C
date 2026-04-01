26dee3d1a729931b8ab97427ef938a37325b792c
João Gabriel MSilva <joao.gabriel.msilva10@gmail.com>
2026-04-01 09:29:11 -0300

    estudo basico
    estudo

---
+++ ativ10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float valor_inicial, valor_final, taxas_juros, anos;
+    taxas_juros = 0.05f; // Taxa de juros fixa de 5% (0.05)
+
+    printf("Digite o valor do seu investimento: ");
+    if (scanf("%f", &valor_inicial) != 1) return 1;
+
+    printf("Digite o numero de anos que voce quer investir: ");
+    if (scanf("%f", &anos) != 1) return 1;
+
+    valor_final = valor_inicial + (valor_inicial * taxas_juros * anos); // juros simples
+    printf("O valor final do seu investimento sera: %.2f\n", valor_final);
+
+    return 0;
+}
